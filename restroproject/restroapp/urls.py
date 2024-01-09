1ca9196f0e222231a7ac1a41fb1372b8df5ff158
supreetvajjarmatti <supreetvajjaramatti@gmail.com>
2024-01-09 18:55:42 +0530

    changes

--- restroproject/restroapp/urls.py
+++ restroproject/restroapp/urls.py
@@ -2,7 +2,17 @@ from django.urls import path
 from .import views
 
 urlpatterns = [
-    
     path('',views.index,name='index'),
+    path('index.html',views.index,name='index'),
+    path('about.html',views.about,name='about'),
+    path('booking.html',views.booking,name='booking'),
+    path('contact.html',views.contact,name='contact'),
+    path('menu.html',views.menu,name='menu'),
+    path('service.html',views.service,name='service'),
+    path('table_booking.html',views.table_booking,name='table_booking'),
+    path('team.html',views.team,name='team'),
+    path('terms&condition.html',views.terms,name='terms'),
+    path('testimonial.html',views.testimonial,name='testimonial'),
+
 
 ]
--- restroproject/restroapp/views.py
+++ restroproject/restroapp/views.py
@@ -1,8 +1,31 @@
 from django.shortcuts import render
 
-# Create your views here.
-
 def index(request):
-    return render(request,'index.html')
+    return render(request, 'index.html')
 
+def about(request):
+    return render(request, 'about.html')
 
+def booking(request):
+    return render(request, 'booking.html')
+
+def contact(request):
+    return render(request, 'contact.html')
+
+def menu(request):
+    return render(request, 'menu.html')
+
+def service(request):
+    return render(request, 'service.html')
+
+def table_booking(request):
+    return render(request, 'table_booking.html')
+
+def team(request):
+    return render(request, 'team.html')
+
+def terms(request):
+    return render(request, 'terms&condition.html')
+
+def testimonial(request):
+    return render(request, 'testimonial.html')
